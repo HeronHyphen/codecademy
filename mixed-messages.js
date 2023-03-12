95df9f87ab5075363edee9b08f1b7671cdb5025d
HeronHyphen <gabegordon03@gmail.com>
2023-03-11 21:23:18 -0600

    Fleshed out the figures of speech arrays, and began work on the createInspirationalQuote function

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,16 +1,16 @@
 // This program generates a nonsensical inspirational message everytime it is ran
 
 const quoteTemplates = [
-  "The secret of getting ahead is getting started.",
-  "It's hard to beat a person who never gives up.",
-  "You can either experience the pain of discipline or the pain of regret. The choice is yours.", 
-  "Great things are done by a series of small things brought together.",
-  "Start where you are. Use what you have. Do what you can.", "If you can't do anything about it then let it go. Don't be a prisoner to things you can't change.",
-  "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
-  "The best way to predict your future is to create it.",
-  "You can't let your failures define you. You have to let your failures teach you.",
-  "Never regret a day in your life. Good days bring you happiness and bad days give you experience.",
-  "Live as if you were to die tomorrow. Learn as if you were to live forever."
+  `A ${n1} without ${n2} is a ${n1} ${ptv}.`,
+  `It's ${adj} to ${v} a person who ${adv} gives up.`,
+  `You can either ${v} the ${n1} of ${n2} or the ${n1} of ${n3}. The choice is yours.`, 
+  `${adj1} things are done by a series of ${adj2} things ${ptv} together.`,
+  `${v1} where you are. ${v2} what you have. ${v3} what you can.", "If you can't do anything about it then let it go. Don't be a ${n1} to things you can't ${v4}.`,
+  `${n1} cannot ${v} ${n1}: only ${n2} can do that. ${n3} cannot ${v} ${n3}: only ${n4} can do that.`,
+  `The ${adv} way to ${v1} your ${n} is to ${v2} it.`,
+  `You can't let your ${n} ${v1} you. You have to ${v2} your ${n} ${v1} you.`,
+  `Never ${v} a day in your life. ${adj1} days bring you happiness and ${adj2} days ${v} you experience.`,
+  `${v1} as if you were to ${v2} tomorrow. ${v3} as if you were to ${v1} forever.`
 ];
 
 const nouns = ['secret', 'person', 'joy', 'pain', 'path', 'life', 'greatness', 'achievement', 'work', 'love', 'hate', 'light',
@@ -21,19 +21,38 @@ const verbs = ['find', 'live', 'go', 'begin', 'end', 'create', 'work', 'satisfy'
 'do', 'fight', 'allow', 'know', 'persevere', 'try', 'fail', 'make', 'miss', 'understand', 'join', 'define', 'be', 'regret', 
 'laugh', 'continue', 'finish', 'resist', 'remember', 'tell', 'say', 'endure', 'experience', 'scare', 'release'];
 
-const adjectives = ['good', 'bad', 'great', 'different', 'new', 'old', 'only', 'best', 'worst', 'next', 'first', 'last', 'low',
+const adjectives = ['good', 'bad', 'great', 'different', 'new', 'old', 'best', 'worst', 'next', 'first', 'last', 'low',
 'high', 'small', 'big', 'large', 'quiet', 'loud', 'soft', 'hard', 'normal', 'exquiste', 'terrifying', 'jarring', 'awesome',
 'mundane', 'fast', 'quick', 'slow', 'languid', 'nervous', 'confident', 'extraordinary', 'common', 'rare', 'unique', 'smart', 
 'stupid', 'average', 'hot', 'cold', 'warm', 'cool', 'mediocre', 'exciting', 'beautiful', 'uncanny', 'familiar', 'sucessful'];
 
+const adverbs = ['only', 'sometimes', 'never', 'always', 'sparingly', 'actively', 'passively'];
+
+const pastTenseVerbs = ['found', 'lived', 'went', 'began', 'ended', 'created', 'worked', 'satisfied', 'recovered', 'won', 'lost',
+'dreamt', 'did', 'fought', 'allowed', 'knew', 'persevered', 'tried', 'feil', 'maked', 'missed', 'understoond', 'joined',
+'defined', 'was', 'regretted', 'laughed', 'continued', 'finished', 'resisted', 'remembered', 'told', 'said', 'endured',
+'experienced', 'scared', 'released'];
 
 const randomNumber = () => Math.random();
 
-function getRandomWord(arr) {
-    // Returns a randon number
-    return arr[Math.round(randomNumber() * arr.length)];
+function getRandomElement(arr) {
+  return arr[Math.round(randomNumber() * arr.length)];
 };
 
-function createInspirationalQuote() {
-
+function createInspirationalQuote() {  
+  const inspirationalQuote = getRandomElement(quoteTemplates);
+    if (inspirationalQuote === quoteTemplates[0]) {
+        let n1 = getRandomElement(nouns);
+        let n2 = getRandomElement(nouns);
+        let ptv = getRandomElement(pastTenseVerbs);
+        return `A ${n1} without ${n2} is a ${n1} ${ptv}.`;
+    } else if (inspirationalQuote === quoteTemplates[1]) {
+        let adj = getRandomElement(adjectives);
+        let v = getRandomElement(verbs);
+        let adv = getRandomElement(adverbs);
+        return `It's ${adj} to ${v} a person who ${adv} gives up.`;   
+    }
 };
+
+let a = "The secret of getting ahead is getting started."
+console.log(a === quoteTemplates[0])
