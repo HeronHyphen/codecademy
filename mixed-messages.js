f935d11b8592a6456d7454f6ff0d560cc1b734b3
HeronHyphen <gabegordon03@gmail.com>
2023-03-11 22:33:58 -0600

    Flseshed out createInspirationalQuote

--- mixed-messages.js
+++ mixed-messages.js
@@ -5,7 +5,7 @@ const quoteTemplates = [
   `It's ${adj} to ${v} a person who ${adv} gives up.`,
   `You can either ${v} the ${n1} of ${n2} or the ${n1} of ${n3}. The choice is yours.`, 
   `${adj1} things are done by a series of ${adj2} things ${ptv} together.`,
-  `${v1} where you are. ${v2} what you have. ${v3} what you can.", "If you can't do anything about it then let it go. Don't be a ${n1} to things you can't ${v4}.`,
+  `${v1} where you are. ${v2} what you have. ${v3} what you can. If you can't do anything about it then let it go. Don't be a ${n} to things you can't ${v4}.`,
   `${n1} cannot ${v} ${n1}: only ${n2} can do that. ${n3} cannot ${v} ${n3}: only ${n4} can do that.`,
   `The ${adv} way to ${v1} your ${n} is to ${v2} it.`,
   `You can't let your ${n} ${v1} you. You have to ${v2} your ${n} ${v1} you.`,
@@ -42,17 +42,71 @@ function getRandomElement(arr) {
 function createInspirationalQuote() {  
   const inspirationalQuote = getRandomElement(quoteTemplates);
     if (inspirationalQuote === quoteTemplates[0]) {
-        let n1 = getRandomElement(nouns);
-        let n2 = getRandomElement(nouns);
-        let ptv = getRandomElement(pastTenseVerbs);
-        return `A ${n1} without ${n2} is a ${n1} ${ptv}.`;
+      let n1 = getRandomElement(nouns);
+      let n2 = getRandomElement(nouns);
+      let ptv = getRandomElement(pastTenseVerbs);
+      return `A ${n1} without ${n2} is a ${n1} ${ptv}.`;
+
     } else if (inspirationalQuote === quoteTemplates[1]) {
-        let adj = getRandomElement(adjectives);
+      let adj = getRandomElement(adjectives);
+      let v = getRandomElement(verbs);
+      let adv = getRandomElement(adverbs);
+      return `It's ${adj} to ${v} a person who ${adv} gives up.`;   
+
+    } else if (inspirationalQuote === quoteTemplates[2]) {
+      let v = getRandomElement(verbs);
+      let n1 = getRandomElement(nouns);
+      let n2 = getRandomElement(nouns);
+      let n3 = getRandomElement(nouns);
+      return `You can either ${v} the ${n1} of ${n2} or the ${n1} of ${n3}. The choice is yours.`
+
+    } else if (inspirationalQuote === quoteTemplates[3]) {
+        let adj1 = getRandomElement(adjectives);
+        let adj2 = getRandomElement(adjectives);
+        let ptv = getRandomElement(pastTenseVerbs);        
+        return  `${adj1} things are done by a series of ${adj2} things ${ptv} together.`;   
+
+    } else if (inspirationalQuote === quoteTemplates[4]) {
+        let v1 = getRandomElement(verbs);
+        let v2 = getRandomElement(verbs);
+        let v3 = getRandomElement(verbs);       
+        let n = getRandomElement(nouns);
+        let v4 = getRandomElement(verbs); 
+        return  `${v1} where you are. ${v2} what you have. ${v3} what you can. 
+        If you can't do anything about it then let it go. Don't be a ${n} to things you can't ${v4}.`;   
+   
+
+    } else if (inspirationalQuote === quoteTemplates[5]) {
+        let n1 = getRandomElement(nouns);
         let v = getRandomElement(verbs);
+        let n2 = getRandomElement(nouns);
+        let n3 = getRandomElement(nouns);
+        let n4 = getRandomElement(nouns);
+        return `${n1} cannot ${v} ${n1}: only ${n2} can do that. ${n3} cannot ${v} ${n3}: only ${n4} can do that.`;    
+
+    } else if (inspirationalQuote === quoteTemplates[6]) {
         let adv = getRandomElement(adverbs);
-        return `It's ${adj} to ${v} a person who ${adv} gives up.`;   
+        let v1 = getRandomElement(verbs);
+        let n = getRandomElement(nouns);
+        let v2 = getRandomElement(verbs);
+        return `The ${adv} way to ${v1} your ${n} is to ${v2} it.`;   
+
+    } else if (inspirationalQuote === quoteTemplates[7]) {
+        let n = getRandomElement(nouns);
+        let v1 = getRandomElement(verbs);
+        let v2 = getRandomElement(verbs);
+        return `You can't let your ${n} ${v1} you. You have to ${v2} your ${n} ${v1} you.`;   
+
+    } else if (inspirationalQuote === quoteTemplates[8]) {
+        let v = getRandomElement(verbs);
+        let adj1 = getRandomElement(adjectives);
+        let adj2 = getRandomElement(adjectives);
+        return `Never ${v} a day in your life. ${adj1} days bring you happiness and ${adj2} days ${v} you experience.`; 
+
+    } else {
+        let v1 = getRandomElement(verbs);
+        let v2 = getRandomElement(verbs);
+        let v3 = getRandomElement(verbs);
+        return `${v1} as if you were to ${v2} tomorrow. ${v3} as if you were to ${v1} forever.`;   
     }
 };
-
-let a = "The secret of getting ahead is getting started."
-console.log(a === quoteTemplates[0])
